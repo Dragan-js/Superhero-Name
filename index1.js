ff8f258469e1235f0086ec6f87c01e4947f87e5a
unknown <dragan.gaming009@gmail.com>
2025-03-15 10:44:45 +0100

    Complete App

---
+++ index1.js
@@ -0,0 +1,14 @@
+import express from "express";
+import superheroes, { randomSuperhero } from "superheroes";
+
+const port = 3000;
+const app = express();
+
+app.get("/", (req, res) => {
+    let superheroName = randomSuperhero();
+    res.send(`<h1>Behold, I am ${superheroName}!</h1>`);
+});
+
+app.listen(port, (req, res) => {
+    console.log(`Server running on port ${port}...`);
+});
